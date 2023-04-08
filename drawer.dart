b6c0c9c8e7253f86d3f7ffc4d6fa3d208c811035
Momoh E. Samuel <118320986+Momohni@users.noreply.github.com>
2023-04-08 06:39:30 -0700

    Update drawer.dart

--- drawer.dart
+++ drawer.dart
@@ -1,90 +1,90 @@
-import 'package:flutter/material.dart';
-import 'package:snackbar_dailogs_bottom_sheets/home_page.dart';
-import 'package:snackbar_dailogs_bottom_sheets/bmi_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:snackbar_dailogs_bottom_sheets/home_page.dart';
+// import 'package:snackbar_dailogs_bottom_sheets/bmi_screen.dart';
 
-class MenuDrawer extends StatefulWidget {
-  const MenuDrawer({super.key});
+// class MenuDrawer extends StatefulWidget {
+//   const MenuDrawer({super.key});
 
-  @override
-  State<MenuDrawer> createState() => _MenuDrawerState();
-}
+//   @override
+//   State<MenuDrawer> createState() => _MenuDrawerState();
+// }
 
-class _MenuDrawerState extends State<MenuDrawer> {
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Colors.black,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          DrawerHeader(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage('assets/drawerimage.jpg'))),
-              child: Text(
-                'Globo Fitness',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 27,
-                  fontWeight: FontWeight.bold,
-                ),
-              )),
-          Expanded(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: [
-                ListTile(
-                  onTap: () {
-                    setState(() {
-                      HomePage();
-                    });
-                    Navigator.of(context).pop();
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) => HomePage()));
-                  },
-                  title: Text('Home',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18
-                  ),),
-                ),
-                ListTile(
-                  onTap: () {
-                    setState(() {
-                      BmiScreen();
-                    });
-                    Navigator.of(context).pop();
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) => BmiScreen()));
-                  },
-                  title: Text('BMI Calculator',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18
-                  ),),
-                ),
-                ListTile(
-                  onTap: () {},
-                  title: Text('Weather',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18
-                  ),),
-                ),
-                ListTile(
-                  onTap: () {},
-                  title: Text('Training',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18
-                  ),),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// class _MenuDrawerState extends State<MenuDrawer> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Drawer(
+//       backgroundColor: Colors.black,
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: [
+//           DrawerHeader(
+//               decoration: BoxDecoration(
+//                   image: DecorationImage(
+//                       fit: BoxFit.fill,
+//                       image: AssetImage('assets/drawerimage.jpg'))),
+//               child: Text(
+//                 'Globo Fitness',
+//                 style: TextStyle(
+//                   color: Colors.white,
+//                   fontSize: 27,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               )),
+//           Expanded(
+//             child: ListView(
+//               padding: EdgeInsets.zero,
+//               children: [
+//                 ListTile(
+//                   onTap: () {
+//                     setState(() {
+//                       HomePage();
+//                     });
+//                     Navigator.of(context).pop();
+//                     Navigator.of(context)
+//                         .push(MaterialPageRoute(builder: (context) => HomePage()));
+//                   },
+//                   title: Text('Home',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 18
+//                   ),),
+//                 ),
+//                 ListTile(
+//                   onTap: () {
+//                     setState(() {
+//                       BmiScreen();
+//                     });
+//                     Navigator.of(context).pop();
+//                     Navigator.of(context)
+//                         .push(MaterialPageRoute(builder: (context) => BmiScreen()));
+//                   },
+//                   title: Text('BMI Calculator',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 18
+//                   ),),
+//                 ),
+//                 ListTile(
+//                   onTap: () {},
+//                   title: Text('Weather',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 18
+//                   ),),
+//                 ),
+//                 ListTile(
+//                   onTap: () {},
+//                   title: Text('Training',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 18
+//                   ),),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
